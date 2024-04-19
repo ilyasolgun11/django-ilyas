2297d5567491b0b43b91d98d8ec84582866dc9ab
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-04-19 22:45:44 +0100

    Debug set back to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=key76kba5-cez#vv#%+cs(3=@az#-g0mkfsd_#bo2ukg-nt%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
