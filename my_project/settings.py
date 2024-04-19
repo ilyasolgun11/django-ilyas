1f94a9a4aa794ed511b53bf7d7db7954672e84d4
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-04-19 23:49:43 +0100

    Add localhost as allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=key76kba5-cez#vv#%+cs(3=@az#-g0mkfsd_#bo2ukg-nt%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '8000']
 
 
 # Application definition
